9b7109f77a44de1d262e547d00a8961488634fbb
Tom De Moor <tomdemoor@pop-os.localdomain>
2021-03-18 21:12:24 +0100

    adding a slashcommand

--- server/plugin.go
+++ server/plugin.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"strings"
 	"sync"
 
+	"github.com/mattermost/mattermost-server/v5/model"
 	"github.com/mattermost/mattermost-server/v5/plugin"
+	"github.com/pkg/errors"
 )
 
 // Plugin implements the interface expected by the Mattermost server to communicate between the server and plugin processes.
@@ -20,9 +22,37 @@ type Plugin struct {
 	configuration *configuration
 }
 
-// ServeHTTP demonstrates a plugin that handles HTTP requests by greeting the world.
-func (p *Plugin) ServeHTTP(c *plugin.Context, w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello, world!")
+// registering a slashcommand /hello
+func (p *Plugin) OnActivate() error {
+
+	if err := p.API.RegisterCommand(&model.Command{
+		Trigger:          "hello",
+		AutoComplete:     true,
+		AutoCompleteHint: "",
+		AutoCompleteDesc: "responds world",
+	}); err != nil {
+		return errors.Wrapf(err, "failed to register  command")
+	}
+	return nil
+
 }
 
-// See https://developers.mattermost.com/extend/plugins/server/reference/
+// adding functionality to to slashcommand
+func (p *Plugin) ExecuteCommand(c *plugin.Context, args *model.CommandArgs) (*model.CommandResponse, *model.AppError) {
+
+	// reading the slashcommand
+	trigger := strings.TrimPrefix(strings.Fields(args.Command)[0], "/")
+	switch trigger {
+	case "hello":
+		return &model.CommandResponse{
+			ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL,
+			Text:         fmt.Sprintf("World"),
+		}, nil
+
+	default:
+		return &model.CommandResponse{
+			ResponseType: model.COMMAND_RESPONSE_TYPE_EPHEMERAL,
+			Text:         fmt.Sprintf("Unknown__ command: " + args.Command),
+		}, nil
+	}
+}
