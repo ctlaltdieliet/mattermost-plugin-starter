6ecaada8235bf46ebbcc540d57c8242cca5b31f6
Tom De Moor <tom@controlaltdieliet.be>
2021-04-26 20:35:49 +0200

    webapp_step 1 initial

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -6,22 +6,7 @@ const Icon = () => <i className='icon fa fa-plug'/>;
 class HelloWorldPlugin {
     initialize(registry, store) {
         
-        registry.registerMainMenuAction(
-            'Plugin Menu Item',
-            () => console.log("Hi"),
-         Icon
-        );
-        registry.registerChannelHeaderButtonAction(
-            // icon - JSX element to use as the button's icon
-            <Icon />,
-            // action - a function called when the button is clicked, passed the channel and channel member as arguments
-            // null,
-            () => {
-                alert("Hello Tom");
-            },
-            // dropdown_text - string or JSX element shown for the dropdown button description
-            "Hello you",
-        );
+      
     }
 }
 
