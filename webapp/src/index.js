67fef17bd44de0242cd1bc4d9f92ec147f6dbad0
Tom De Moor <tom@controlaltdieliet.be>
2021-05-04 14:37:45 +0200

    Toggling the right sidebar

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -6,27 +6,20 @@ const Icon = () => <i className='icon fa fa-plug'/>;
 class HelloWorldPlugin {
     initialize(registry, store) {
         registry.registerMainMenuAction(
-          "Saying hi",
-          () => allert("Hi again"),
+            "Saying hi",
+            () => allert("Hi again"),
         );
+        
         registry.registerChannelHeaderButtonAction(
-            // icon - JSX element to use as the button's icon
             <Icon />,
-            // action - a function called when the button is clicked, passed the channel and channel member as arguments
-            // null,
-          
-                () => store.dispatch(toggleRHSPlugin),
-          
-            // dropdown_text - string or JSX element shown for the dropdown button description
+            () => store.dispatch(toggleRHSPlugin),
             "Says hello",
         );
 
         const {toggleRHSPlugin} = registry.registerRightHandSidebarComponent(
             RightSideBar,"Applause!");
-  
-            const {leftsidebarheader}=registry.registerLeftSidebarHeaderComponent(MyFirstComponent)
-            
-    
+        
+        const {leftsidebarheader}=registry.registerLeftSidebarHeaderComponent(MyFirstComponent)
     }
 }
 
