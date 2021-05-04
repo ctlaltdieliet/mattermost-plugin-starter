5eeed14891a44c1d17817adf6088610722f370df
Tom De Moor <tom@controlaltdieliet.be>
2021-05-04 14:41:22 +0200

    typo fix

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -9,14 +9,10 @@ class HelloWorldPlugin {
           () => alert("Hi again"),
         );
         registry.registerChannelHeaderButtonAction(
-            // icon - JSX element to use as the button's icon
             <Icon />,
-            // action - a function called when the button is clicked, passed the channel and channel member as arguments
-            // null,
             () => {
                 alert("Hi there");
             },
-            // dropdown_text - string or JSX element shown for the dropdown button description
             "Says hello",
         );
     }
