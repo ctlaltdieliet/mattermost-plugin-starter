b9ca84675563c9163f4638584866b7895d741c6f
Tom De Moor <tom@controlaltdieliet.be>
2021-05-04 12:46:28 +0200

    adding to index.js

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import MyFirstComponent from './components/my_first_component'
 const Icon = () => <i className='icon fa fa-plug'/>;
 class HelloWorldPlugin {
     initialize(registry, store) {
+        const {leftsidebarheader}=registry.registerLeftSidebarHeaderComponent(MyFirstComponent)
         registry.registerMainMenuAction(
           "Saying hi",
           () => alert("Hi again"),
